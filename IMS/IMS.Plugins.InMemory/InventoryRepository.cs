0a6013f2c7f75f607b80b363ec0fc88bb93186bd
Mulhamsth <mulhamsth@gmail.com>
2023-04-04 00:41:10 +0200

    costumizing the homepage and adding some inventories

--- IMS/IMS.Plugins.InMemory/InventoryRepository.cs
+++ IMS/IMS.Plugins.InMemory/InventoryRepository.cs
@@ -15,7 +15,11 @@ namespace IMS.Plugins.InMemory
                 new Inventory{InventoryId = 1, InventoryName ="Bike Seat", Quantity = 10, Price = 2},
                 new Inventory{InventoryId = 2, InventoryName ="Bike Body", Quantity = 10, Price = 15},
                 new Inventory{InventoryId = 3, InventoryName ="Bike Wheels", Quantity = 20, Price = 8},
-                new Inventory{InventoryId = 4, InventoryName ="Bike Pedels", Quantity = 20, Price = 1}
+                new Inventory{InventoryId = 4, InventoryName ="Bike Pedels", Quantity = 20, Price = 1},
+                new Inventory{InventoryId = 5, InventoryName ="Giant C1285 MTB Sport 26\" Tire", Quantity = 4, Price = 21},
+                new Inventory{InventoryId = 5, InventoryName ="Serfas Dual Density Men's Saddle", Quantity = 7, Price = 44.99},
+                new Inventory{InventoryId = 5, InventoryName ="Premium Tube Schrader Valve", Quantity = 1, Price = 7.99},
+                new Inventory{InventoryId = 5, InventoryName ="Premium Tube Presta Valve", Quantity = 9, Price = 7.99},
             };
         }
 
